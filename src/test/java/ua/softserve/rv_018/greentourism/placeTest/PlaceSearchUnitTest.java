196aae0b44b81ff35750e4d7895a204dfbb51424
47 souls <akluyuko@gmail.com>
2016-08-12 12:50:47 +0400

    Unit test on searching Place possibility

---
+++ src/test/java/ua/softserve/rv_018/greentourism/placeTest/PlaceSearchUnitTest.java
@@ -0,0 +1,50 @@
+package ua.softserve.rv_018.greentourism.placeTest;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import ua.softserve.rv_018.greentourism.model.Place;
+import ua.softserve.rv_018.greentourism.service.PlaceService;
+import ua.softserve.rv_018.greentourism.service.PlaceServiceImpl;
+
+public class PlaceSearchUnitTest {
+
+	@Mock
+	private Place place;
+	@Mock
+	private PlaceServiceImpl service;
+	public static final String PLACE_NAME = "Name of place";
+	public static final String EMPTY_NAME = "";
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testSearchPlaceByNameThatExists() {
+		when(place.getName()).thenReturn(PLACE_NAME);
+		when(service.findPlaceByName(PLACE_NAME)).thenReturn(place);
+		assertEquals(place.getName(), service.findPlaceByName(PLACE_NAME)
+				.getName());
+		assertEquals(place, service.findPlaceByName(PLACE_NAME));
+	}
+
+	@Test
+	public void testSearchPlaceByNameThatDoesNotExist() {
+		when(service.findPlaceByName(PLACE_NAME)).thenReturn(null);
+		assertNotEquals(place, service.findPlaceByName(PLACE_NAME));
+	}
+
+	@Test
+	public void testSearchPlaceByEmptyName() {
+		when(service.findPlaceByName(EMPTY_NAME)).thenReturn(null);
+		assertNotEquals(place, service.findPlaceByName(PLACE_NAME));
+	}
+}
