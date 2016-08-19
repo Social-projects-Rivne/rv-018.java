d49d7e99e1e1ec9f60afbe57335b51592d459562
47 souls <akluyuko@gmail.com>
2016-08-19 10:49:18 +0400

    Changed method name

--- src/main/java/ua/softserve/rv_018/greentourism/service/PlaceService.java
+++ src/main/java/ua/softserve/rv_018/greentourism/service/PlaceService.java
@@ -3,5 +3,5 @@ package ua.softserve.rv_018.greentourism.service;
 import ua.softserve.rv_018.greentourism.model.Place;
 
 public interface PlaceService {
-	Place findPlaceByName(String name);
+	Place findByName(String name);
 }
