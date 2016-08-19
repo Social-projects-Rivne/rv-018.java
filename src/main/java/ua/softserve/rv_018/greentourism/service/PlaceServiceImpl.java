e7f6392d0c3d37619fc412e19feece56640994b7
47 souls <akluyuko@gmail.com>
2016-08-19 10:25:58 +0400

    Provided implementation

--- src/main/java/ua/softserve/rv_018/greentourism/service/PlaceServiceImpl.java
+++ src/main/java/ua/softserve/rv_018/greentourism/service/PlaceServiceImpl.java
@@ -1,13 +1,26 @@
 package ua.softserve.rv_018.greentourism.service;
 
-import ua.softserve.rv_018.greentourism.model.Place;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import ua.softserve.rv_018.greentourism.model.Place;
+import ua.softserve.rv_018.greentourism.repository.PlaceRepository;
+
+@Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class PlaceServiceImpl implements PlaceService {
+
+	@Autowired
+	private PlaceRepository placeRepository;
 
 	@Override
 	public Place findPlaceByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		Place place = placeRepository.findByName(name);
+		return place;
 	}
 
 }
