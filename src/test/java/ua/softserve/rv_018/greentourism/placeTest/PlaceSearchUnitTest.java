3a8cd803bf48f98eccdfc4a84f588584b9380da7
47 souls <akluyuko@gmail.com>
2016-08-19 20:23:16 +0400

    Test were changed. Instead of testing one instance they are now testing array of instances

--- src/test/java/ua/softserve/rv_018/greentourism/placeTest/PlaceSearchUnitTest.java
+++ src/test/java/ua/softserve/rv_018/greentourism/placeTest/PlaceSearchUnitTest.java
@@ -3,6 +3,9 @@ package ua.softserve.rv_018.greentourism.placeTest;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -17,32 +20,30 @@ public class PlaceSearchUnitTest {
 	private Place place;
 	@Mock
 	private PlaceServiceImpl service;
+	private static List<Place> places;
 	public static final String PLACE_NAME = "Name of place";
 	public static final String EMPTY_NAME = "";
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		places = new ArrayList<Place>();
+		places.add(place);
+		places.add(place);
+		places.add(place);
 	}
 
 	@Test
 	public void testSearchPlaceByNameThatExists() {
-		when(place.getName()).thenReturn(PLACE_NAME);
-		when(service.findByName(PLACE_NAME)).thenReturn(place);
-		assertEquals(place.getName(), service.findByName(PLACE_NAME)
-				.getName());
-		assertEquals(place, service.findByName(PLACE_NAME));
+		when(service.findByName(PLACE_NAME)).thenReturn(places);
+		assertEquals(true,
+				(service.findByName(PLACE_NAME).containsAll(places)&& places.containsAll(service.findByName(PLACE_NAME))));
 	}
 
 	@Test
-	public void testSearchPlaceByNameThatDoesNotExist() {
-		when(service.findByName(PLACE_NAME)).thenReturn(null);
-		assertNotEquals(place, service.findByName(PLACE_NAME));
+	public void testSearchPlaceByNameThatDoesNotExistAndByEmptyName() {
+		when(service.findByName(PLACE_NAME)).thenReturn(new ArrayList<Place>());
+		assertNotEquals(places, service.findByName(PLACE_NAME));
 	}
 
-	@Test
-	public void testSearchPlaceByEmptyName() {
-		when(service.findByName(EMPTY_NAME)).thenReturn(null);
-		assertNotEquals(place, service.findByName(PLACE_NAME));
-	}
 }
