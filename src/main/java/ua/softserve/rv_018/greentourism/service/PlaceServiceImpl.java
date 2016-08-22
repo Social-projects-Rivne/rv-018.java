9800e5386c0f3a244a4f135fcc5fa8ffb0532425
47 souls <akluyuko@gmail.com>
2016-08-22 12:35:05 +0400

    Mistake when deleted @Service annotation

--- src/main/java/ua/softserve/rv_018/greentourism/service/PlaceServiceImpl.java
+++ src/main/java/ua/softserve/rv_018/greentourism/service/PlaceServiceImpl.java
@@ -5,12 +5,14 @@ import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import ua.softserve.rv_018.greentourism.model.Place;
 import ua.softserve.rv_018.greentourism.repository.PlaceRepository;
 
+@Service
 @Transactional(
         propagation = Propagation.SUPPORTS,
         readOnly = true)
