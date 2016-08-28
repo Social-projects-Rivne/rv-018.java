6908a265e7208b2c8beee5ab54af3884cde1194b
Ihor Kazakov <ihorkazakov@gmail.com>
2016-08-28 19:59:53 +0300

    added java method for adding feedback(comments)

---
+++ src/main/java/ua/softserve/rv_018/greentourism/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package ua.softserve.rv_018.greentourism.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ua.softserve.rv_018.greentourism.model.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
