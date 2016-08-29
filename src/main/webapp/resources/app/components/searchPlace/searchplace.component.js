82ff2622490a3836e27b4b22f4d11fdfc035bfed
KostyaTk <css.lucky966@gmail.com>
2016-08-29 14:44:08 +0300

    small changes

--- src/main/webapp/resources/app/components/searchPlace/searchplace.component.js
+++ src/main/webapp/resources/app/components/searchPlace/searchplace.component.js
@@ -5,11 +5,13 @@ angular.
     component('searchplace', {
       templateUrl: _contextPath + '/resources/app/components/searchPlace/searchplace.template.html',
       controller: ['$scope', '$http', '$routeParams', '$rootScope', function($scope, $http, $routeParams, $rootScope) {
-    	$scope.searchPlacesSidebarOpen = true;
+    	$scope.searchPlacesSidebarOpen = false;
     	
     	$http.get(_contextPath + '/api/place/filter/name?name=' + $routeParams.name + '&ignorecase=true').then(function(response) {
   		  $scope.places = response.data;
-  		  console.log("request")
+  		  
+  		  if (response.data.length != 0)
+  			$scope.searchPlacesSidebarOpen = true;  
     	});
     	
     	$scope.closePlacesSidebar = function() {
@@ -17,7 +19,6 @@ angular.
     	}
     	
     	$scope.refocusMap = function(latitude, longitude) {
-    		console.log("inside refocus")
     		$rootScope.myMap.panTo(new L.LatLng(latitude, longitude));
     	}
       }]
