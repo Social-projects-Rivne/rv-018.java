7188c08a6c6f23e3624439ca426fc4aef879f252
ihorkazakov <ihorkazakov@gmail.com>
2016-08-29 11:51:00 +0300

    fix mistake in CommentRepository

--- src/main/java/ua/softserve/rv_018/greentourism/repository/CommentRepository.java
+++ src/main/java/ua/softserve/rv_018/greentourism/repository/CommentRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ua.softserve.rv_018.greentourism.model.Comment;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
 }
