0eb6610caff8feb728038f2c8f945cef01c06560
Maksim <bmaksmail@gmail.com>
2016-08-30 18:02:02 +0300

    Bugfix.

--- src/main/webapp/resources/app/marker.controller.js
+++ src/main/webapp/resources/app/marker.controller.js
@@ -5,18 +5,84 @@ angular.module('greenApp')
 		var markersArray = [];
     	var currentlySelectedTabInnerHtml = "Places";
 		
-		var myMapMoveEndEventFunc = function() {
+		$rootScope.$on('initMarkerController', function() {
+			$rootScope.myMap.on('moveend', function() {
+				var latLngBounds = $rootScope.myMap.getBounds();
+				var urlPath = currentlySelectedTabInnerHtml.toLowerCase().substring(0, currentlySelectedTabInnerHtml.length - 1);
+				
+				$scope.progressBarVision = true;
+				$http({
+					method: 'GET',
+					url: _contextPath + '/api/' 
+										+ urlPath  
+										+ '/point' 
+										+ '?south-west=' + latLngBounds.getSouthWest().lat + ':' + latLngBounds.getSouthWest().lng 
+										+ '&north-east=' + latLngBounds.getNorthEast().lat + ':' + latLngBounds.getNorthEast().lng
+				})
+				.then(function(response){
+					$scope.progressBarVision = false;
+					var points = response.data;
+
+					if (markersArray.length > 0)
+						angular.forEach(markersArray, function(marker, key){
+							$rootScope.myMap.removeLayer(marker);
+						})
+					markersArray = [];
+					
+					var markerIcon = null;
+					if (urlPath == "place") {
+						var BlueIcon = L.Icon.Default.extend ({
+							options: {
+								iconUrl: 'http://www.clker.com/cliparts/q/I/Q/u/Z/1/marker-hi.png'
+							}
+						});
+						markerIcon = new BlueIcon(); 
+					} else if (urlPath = "event") {
+						var GreenIcon = L.Icon.Default.extend ({
+							options: {
+								iconUrl: 'http://www.clker.com/cliparts/G/e/o/0/f/m/map-pin-red-hi.png'
+							}
+						});
+						markerIcon = new GreenIcon(); 					
+					}
+					
+					angular.forEach(points, function(point, key){
+						markersArray.push(L.marker([point.latitude, point.longitude], {icon: markerIcon}).addTo($rootScope.myMap));
+					})
+				}, function(error){
+					$scope.progressBarVision = false;
+					$rootScope.error = error;
+					$log.info(error);
+				});
+
+				angular.forEach(markersArray, function(marker, key){
+					$rootScope.myMap.addLayer(marker);
+				})
+			});
+		})
+		
+		// Check currently selected tab
+		$('ul.tabs').on('click', 'a', function(e) {
+			currentlySelectedTabInnerHtml = e.currentTarget.innerHTML;
+			
+			if (markersArray.length > 0)
+				angular.forEach(markersArray, function(marker, key){
+					$rootScope.myMap.removeLayer(marker);
+				})
+				
+			markersArray = [];
+
 			var latLngBounds = $rootScope.myMap.getBounds();
 			var urlPath = currentlySelectedTabInnerHtml.toLowerCase().substring(0, currentlySelectedTabInnerHtml.length - 1);
-			
+				
 			$scope.progressBarVision = true;
 			$http({
 				method: 'GET',
 				url: _contextPath + '/api/' 
-									+ urlPath  
-									+ '/point' 
-									+ '?south-west=' + latLngBounds.getSouthWest().lat + ':' + latLngBounds.getSouthWest().lng 
-									+ '&north-east=' + latLngBounds.getNorthEast().lat + ':' + latLngBounds.getNorthEast().lng
+					+ urlPath  
+					+ '/point' 
+					+ '?south-west=' + latLngBounds.getSouthWest().lat + ':' + latLngBounds.getSouthWest().lng 
+					+ '&north-east=' + latLngBounds.getNorthEast().lat + ':' + latLngBounds.getNorthEast().lng
 			})
 			.then(function(response){
 				$scope.progressBarVision = false;
@@ -26,8 +92,8 @@ angular.module('greenApp')
 					angular.forEach(markersArray, function(marker, key){
 						$rootScope.myMap.removeLayer(marker);
 					})
-				markersArray = [];
-				
+					markersArray = [];
+					
 				var markerIcon = null;
 				if (urlPath == "place") {
 					var BlueIcon = L.Icon.Default.extend ({
@@ -44,7 +110,7 @@ angular.module('greenApp')
 					});
 					markerIcon = new GreenIcon(); 					
 				}
-				
+					
 				angular.forEach(points, function(point, key){
 					markersArray.push(L.marker([point.latitude, point.longitude], {icon: markerIcon}).addTo($rootScope.myMap));
 				})
@@ -57,22 +123,5 @@ angular.module('greenApp')
 			angular.forEach(markersArray, function(marker, key){
 				$rootScope.myMap.addLayer(marker);
 			})
-		}
-		
-		$rootScope.$on('initMarkerController', function() {
-			$rootScope.myMap.on('moveend', myMapMoveEndEventFunc());
-		})
-		
-		// Check currently selected tab
-		$('ul.tabs').on('click', 'a', function(e) {
-			currentlySelectedTabInnerHtml = e.currentTarget.innerHTML;
-			
-			if (markersArray.length > 0)
-				angular.forEach(markersArray, function(marker, key){
-					$rootScope.myMap.removeLayer(marker);
-				})
-				
-			markersArray = [];
-			myMapMoveEndEventFunc();
 		});
 });
