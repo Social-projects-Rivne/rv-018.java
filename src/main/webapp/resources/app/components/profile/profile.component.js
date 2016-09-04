6dea8e6c04db96e5f3d8ae59a49cc15e43e19220
Ihor Kazakov <ihorkazakov@gmail.com>
2016-09-04 21:31:37 +0300

    fix mistake with additional alert after pop up

--- src/main/webapp/resources/app/components/profile/profile.component.js
+++ src/main/webapp/resources/app/components/profile/profile.component.js
@@ -7,12 +7,9 @@ component('profile', {
 	    	
 		var successCallBack = function(response){
     		$scope.places = response.data;
-//    		$scope.placeFoto = response.data.placeFoto;
-			
 	    };
 	    
 		$http.get(_contextPath + '/api/place/user/' + $routeParams.id).then(successCallBack);
-		
 		
 		var successCallBack = function(response){
     		$scope.username = response.data.username;
@@ -21,15 +18,9 @@ component('profile', {
 			$scope.lastName = response.data.lastName;
 			$scope.socialAccount = response.data.socialAccount;
 			$scope.userpic = response.data.userpic;
-			
 	    };
 	    
 	    $scope.update = function () {
-	    	// update only if id is specified
-	    	/*if ($scope.id == undefined) {
-	    		$scope.errorMessage = "Please, select the id of existing user and try again!"
-	    		return;
-	    	}*/
 			$scope.id = $routeParams.id;
 	        
 	    	var dataObj = {  
@@ -64,18 +55,12 @@ component('profile', {
 				$scope.lastName = response.data.lastName;
 				$scope.socialAccount = response.data.socialAccount;
 				$scope.userpic = response.data.userpic;
-				
 		    };
 			
 			$http.get(_contextPath + '/user/' + $routeParams.id).then(successCallBack);
 		};
 		
 		$scope.update = function () {
-	    	// update only if id is specified
-	    	/*if ($scope.id == undefined) {
-	    		$scope.errorMessage = "Please, select the id of existing user and try again!"
-	    		return;
-	    	}*/
 			$scope.id = $routeParams.id;
 	        
 	    	var dataObj = {
@@ -94,24 +79,10 @@ component('profile', {
 			});
 		};
 		
-		
 	$(document).ready(function(){
-		  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 		  $('.modal-trigger').leanModal();
 	});
-
-	$('.modal-trigger').leanModal({
-		  dismissible: true, // Modal can be dismissed by clicking outside of the modal
-		  opacity: .1, // Opacity of modal background
-		  in_duration: 300, // Transition in duration
-		  out_duration: 200, // Transition out duration
-		  starting_top: '4%', // Starting top style attribute
-		  ending_top: '10%', // Ending top style attribute
-		  ready: function() { alert('Ready'); }, // Callback for Modal open
-		  complete: function() { alert('Closed'); } // Callback for Modal close
-		}
-		);
 		
-		$scope.ImageUrl="http://content.screencast.com/users/kazakov/folders/Snagit/media/9777b814-7f03-40b4-bafd-c64a0d39e95c/08.31.2016-23.23.png";
+	$scope.ImageUrl="http://content.screencast.com/users/kazakov/folders/Snagit/media/9777b814-7f03-40b4-bafd-c64a0d39e95c/08.31.2016-23.23.png";
 	}
 });
