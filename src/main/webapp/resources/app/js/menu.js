106413a406bc1d06ceb660ee516d389f4c18df19
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-09 23:08:33 +0400

    Just a test case of appearing login form on page. Would be replaced by angular animation

--- src/main/webapp/resources/app/js/menu.js
+++ src/main/webapp/resources/app/js/menu.js
@@ -1,10 +1,25 @@
 (function(){
                 var body = $('body'),
                     menuButton = body.find('#toggle-button'),
-                    content = body.find('.content'),
                     menuList = body.find('.not-active');
                 menuButton.on('click', function(e){
-                    content.toggleClass('transf-content');
                     menuList.toggleClass('active-ul');
                 });
             })();
+function myFunction() {
+    document.getElementById("form").classList.toggle("login-form-active");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
