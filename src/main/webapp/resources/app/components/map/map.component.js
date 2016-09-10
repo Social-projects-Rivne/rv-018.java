bffabbc81a36b14923b4e5cbeeb48a9365c84a9b
Ihor Kazakov <ihorkazakov@gmail.com>
2016-09-11 01:27:51 +0300

    fixed state before use datapicker

--- src/main/webapp/resources/app/components/map/map.component.js
+++ src/main/webapp/resources/app/components/map/map.component.js
@@ -144,6 +144,16 @@ angular.module('greenApp')
       $scope.addEventMenuIsOpen = false;
     };
 
+    $scope.resetAddEventForm = function(form) {
+      $rootScope.myMap.removeLayer(marker);
+    };
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
+
 
     }
 });
