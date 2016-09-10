11f788b5af3ff4c149f6e463dcf1f683c06a54d0
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-10 12:49:47 +0400

    User name need to be unique also as an email

--- src/main/java/ua/softserve/rv_018/greentourism/model/User.java
+++ src/main/java/ua/softserve/rv_018/greentourism/model/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    @Column(name = "username")
+    @Column(name = "username", unique=true)
     private String username;
     @Column(name = "email", unique=true, nullable = false)
     private String email;
