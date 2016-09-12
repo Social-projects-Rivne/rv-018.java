1992079b2a1c363b62d3b76bed24246b27300756
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-12 09:44:42 +0400

    Added config folder, where all application configuration will appear. It consist of DispatcherServlet config, DataSource config, WebApp config and soon will have Security config

---
+++ src/main/java/ua/softserve/rv_018/greentourism/config/WebConfig.java
@@ -0,0 +1,33 @@
+package ua.softserve.rv_018.greentourism.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "ua.softserve.rv_018.greentourism.config", "ua.softserve.rv_018.greentourism.controller",
+		"ua.softserve.rv_018.greentourism.service", "ua.softserve.rv_018.greentourism.repository" })
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+
+		return resolver;
+	}
+
+}
