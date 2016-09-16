37626d0b526431856b9892a295d9b102102b48d5
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-16 08:40:28 +0400

    Changes to have both login and logout possibility

--- src/main/webapp/resources/app/login.controller.js
+++ src/main/webapp/resources/app/login.controller.js
@@ -3,16 +3,31 @@
 angular.module('greenApp').controller('loginController', function($scope, $http, $location) {
 	
 	$scope.login = function() {
+		console.log("In login function");
 		$http({
 			method: 'GET',
 			url: _contextPath + "/login" + "?email=" + $scope.email + "&password=" + $scope.password
 		})
 		.then(function(response){
+			console.log("Success in login function");
 			let user = response.data;
 			$location.path("/profile/" + user.id);
 			console.log(user);
 		}, function(error){
-			
+			console.log("Error in login function");
+			console.log(error.data);
+		});
+	}
+	$scope.logout = function() {
+		console.log("In logout function");
+		$http({
+			method: 'GET',
+			url: _contextPath + "/logout"
+		})
+		.then(function(response){
+			console.log("Success in logout function");
+		}, function(error){
+			console.log("Error in logout function");
 			console.log(error.data);
 		});
 	}
