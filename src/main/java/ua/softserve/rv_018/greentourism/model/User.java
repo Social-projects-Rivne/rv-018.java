8886ccf0a47ab36e8d3dbe587f37d7b7266bbbd1
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-16 08:36:57 +0400

    Modified user table as it requires from mockup

--- src/main/java/ua/softserve/rv_018/greentourism/model/User.java
+++ src/main/java/ua/softserve/rv_018/greentourism/model/User.java
@@ -1,88 +1,95 @@
 package ua.softserve.rv_018.greentourism.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user_detail")
 public class User {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    @Column(name = "username", unique=true)
-    private String username;
-    @Column(name = "email", unique=true, nullable = false)
-    private String email;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    @Column(name = "password", nullable = false)
-    private String password;
-    @Column(name = "is_active", nullable = false, columnDefinition = "boolean default false")
-    private boolean isActive;
-    @Column(name = "social_account")
-    private String socialAccount;
-    @Column(name = "userpic")
-    private String userpic;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "username", unique = true)
+	private String username;
+	@Column(name = "email", unique = true, nullable = false)
+	private String email;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "password", nullable = false)
+	private String password;
+	@Column(name = "is_active", nullable = false, columnDefinition = "boolean default false")
+	private boolean isActive;
+	@Column(name = "social_account")
+	private String socialAccount;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "role_id", nullable = false)
+	private Role role;
+	@Column(name = "userpic")
+	private String userpic;
 
-    public User() {}
+	public User() {
+	}
 
-    public User(Long id, String username, String email, String password) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+	public User(Long id, String username, String email, String password) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 
-    public User(User user) {
-        super();
-        this.id = user.getId();
-        this.username = user.getUsername();
-        this.email = user.getEmail();
-        this.password = user.getPassword();
-        this.firstName = user.getFirstName();
-        this.lastName = user.getLastName();
-        this.isActive = user.isActive();
-    }
+	public User(User user) {
+		super();
+		this.id = user.getId();
+		this.username = user.getUsername();
+		this.email = user.getEmail();
+		this.password = user.getPassword();
+		this.firstName = user.getFirstName();
+		this.lastName = user.getLastName();
+		this.isActive = user.isActive();
+	}
 
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public boolean isActive() {
-        return isActive;
-    }
+	public boolean isActive() {
+		return isActive;
+	}
 
-    public void setActive(boolean isActive) {
+	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
-    
-    public String getEmail() {
+
+	public String getEmail() {
 		return email;
 	}
 
@@ -105,7 +112,7 @@ public class User {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public String getSocialAccount() {
 		return socialAccount;
 	}
@@ -113,7 +120,7 @@ public class User {
 	public void setSocialAccount(String socialAccount) {
 		this.socialAccount = socialAccount;
 	}
-	
+
 	public String getUserpic() {
 		return userpic;
 	}
@@ -122,14 +129,19 @@ public class User {
 		this.userpic = userpic;
 	}
 
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 	@Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", isActive=" + isActive +
-                "}";
-    }
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", password=" + password + ", isActive=" + isActive + ", socialAccount="
+				+ socialAccount + ", role_id=" + role + "]";
+	}
+
 }
