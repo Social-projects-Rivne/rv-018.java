d3395e264f0d64b8c0c1a63c9299e4d9bafd34f5
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-17 21:10:01 +0400

    ng-bind possibility to retrieve user name and user picture in future

--- src/main/webapp/resources/app/menu.controller.js
+++ src/main/webapp/resources/app/menu.controller.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('greenApp').controller('menuController', function($scope) {
-	
+
 	$scope.checkTabs = false;
+	
+	$scope.name = "Oracle";
 	
 	$scope.showTabs = function() {
 		$scope.checkTabs = true;
-		console.log("ShowTabs: $scope.checkTabs = " + $scope.checkTabs);
 	};
 	$scope.hideTabs = function() {
 		$scope.checkTabs = false;
-		console.log("HideTabs: $scope.checkTabs = " + $scope.checkTabs);
 	};
 
 });
