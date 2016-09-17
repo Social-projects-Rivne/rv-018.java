b3f963e2004cb174af81bd96a45bf501dff87fc3
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-17 21:04:16 +0400

    Minor changes to have user id in reponse

--- src/main/java/ua/softserve/rv_018/greentourism/config/AppAuthenticationProvider.java
+++ src/main/java/ua/softserve/rv_018/greentourism/config/AppAuthenticationProvider.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -20,7 +19,7 @@ import ua.softserve.rv_018.greentourism.service.UserService;
 public class AppAuthenticationProvider implements AuthenticationProvider {
 
 	private List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
-	
+
 	@Autowired
 	private UserService userService;
 
@@ -29,20 +28,21 @@ public class AppAuthenticationProvider implements AuthenticationProvider {
 		String email = authentication.getName();
 		User user = userService.findByEmail(email);
 		if (user == null) {
-			System.out.println("User doesn't exist!");
+			return null;
 		}
 		String expectedUserPassword = user.getPassword();
 		AUTHORITIES.add(new SimpleGrantedAuthority(user.getRole().getName()));
 		if (authentication.getCredentials().equals(expectedUserPassword)) {
-			return new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials(),
-					AUTHORITIES);
-		}
-		throw new BadCredentialsException("Bad Credentials");
+			UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+					authentication.getName(), authentication.getCredentials(), AUTHORITIES);
+			token.setDetails(user.getId());
+			return token;
+		} else
+			return null;
 	}
 
 	@Override
 	public boolean supports(Class<?> arg0) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
