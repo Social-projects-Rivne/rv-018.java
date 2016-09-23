8a6b1b66bae65b0412cefefd7464538fc3636445
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-23 10:21:45 +0400

    Default FilterChain initializer

---
+++ src/main/java/ua/softserve/rv_018/greentourism/config/SecurityWebInitializer.java
@@ -0,0 +1,6 @@
+package ua.softserve.rv_018.greentourism.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
