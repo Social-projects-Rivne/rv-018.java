c9c976b26264418f7128e1352baa43b7d7079966
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-23 23:32:34 +0400

    Authentication manager which will be containing as many AuthenticationProviders as it is needed

--- src/main/java/ua/softserve/rv_018/greentourism/config/AppAuthenticationManager.java
+++ src/main/java/ua/softserve/rv_018/greentourism/config/AppAuthenticationManager.java
@@ -1,48 +1,22 @@
 package ua.softserve.rv_018.greentourism.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import ua.softserve.rv_018.greentourism.model.User;
-import ua.softserve.rv_018.greentourism.service.UserService;
+import ua.softserve.rv_018.greentourism.config.authentication.UsernamePasswordAuthenticationProvider;
 
 @Component
-public class AppAuthenticationProvider implements AuthenticationProvider {
-
-	private List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
-
+public class AppAuthenticationManager implements AuthenticationManager {
+	
 	@Autowired
-	private UserService userService;
+	private UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		String email = authentication.getName();
-		User user = userService.findByEmail(email);
-		if (user == null) {
-			return null;
-		}
-		String expectedUserPassword = user.getPassword();
-		AUTHORITIES.add(new SimpleGrantedAuthority(user.getRole().getName()));
-		if (authentication.getCredentials().equals(expectedUserPassword)) {
-			UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-					authentication.getName(), authentication.getCredentials(), AUTHORITIES);
-			token.setDetails(user.getId());
-			return token;
-		} else
-			return null;
+		return usernamePasswordAuthenticationProvider.authenticate(authentication);
 	}
 
-	@Override
-	public boolean supports(Class<?> arg0) {
-		return false;
-	}
 }
