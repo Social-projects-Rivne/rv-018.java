2cc778bf54a902a9fc437e689529d4f383a3cd7a
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-23 23:35:30 +0400

    One of possible AuthenticationProviders

--- src/main/java/ua/softserve/rv_018/greentourism/config/authentication/UsernamePasswordAuthenticationProvider.java
+++ src/main/java/ua/softserve/rv_018/greentourism/config/authentication/UsernamePasswordAuthenticationProvider.java
@@ -1,17 +1,29 @@
 package ua.softserve.rv_018.greentourism.config.authentication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import ua.softserve.rv_018.greentourism.model.User;
+import ua.softserve.rv_018.greentourism.service.UserService;
 
 @Component
 public class UsernamePasswordAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
+	private List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+	
+	@Autowired
+	private UserService userService;
+	
 	@Autowired
 	private TokenUtil tokenUtil;
 
@@ -43,4 +55,22 @@ public class UsernamePasswordAuthenticationProvider extends AbstractUserDetailsA
         return parsedUser;
 	}
 
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String email = authentication.getName();
+		User user = userService.findByEmail(email);
+		if (user == null) {
+			return null;
+		}
+		String expectedUserPassword = user.getPassword();
+		AUTHORITIES.add(new SimpleGrantedAuthority(user.getRole().getName()));
+		if (authentication.getCredentials().equals(expectedUserPassword)) {
+			UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+					authentication.getName(), authentication.getCredentials(), AUTHORITIES);
+			token.setDetails(user.getId());
+			return token;
+		} else
+			return null;
+	}
+	
 }
