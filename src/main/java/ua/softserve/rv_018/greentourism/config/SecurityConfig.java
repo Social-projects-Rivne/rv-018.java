463a82af67749ff4b87fa16e4b3fb8e7f3eb63c6
Andrey Kluyuko <akluyuko@gmail.com>
2016-09-23 23:33:15 +0400

    SecurityConfig with most needed configuration

--- src/main/java/ua/softserve/rv_018/greentourism/config/SecurityConfig.java
+++ src/main/java/ua/softserve/rv_018/greentourism/config/SecurityConfig.java
@@ -1,11 +1,41 @@
 package ua.softserve.rv_018.greentourism.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import ua.softserve.rv_018.greentourism.config.authentication.AppAuthenticationFilter;
+import ua.softserve.rv_018.greentourism.config.authentication.UsernamePasswordAuthenticationEntryPoint;
+
+@ComponentScan("ua.softserve.rv_018.greentourism.config")
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	private UsernamePasswordAuthenticationEntryPoint usernamePasswordAuthenticationEntryPoint;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/logout").permitAll();
+		http.csrf().disable().exceptionHandling().authenticationEntryPoint(usernamePasswordAuthenticationEntryPoint)
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(usernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	
+	
+	@Bean
+	public AppAuthenticationFilter usernamePasswordAuthenticationFilter(){
+		return new AppAuthenticationFilter();
+	}
 }
