bc14d37bdbb7ddbd73a50280a3997ce9bb8dff28
Andrew Kluyuko <akluyuko@gmail.com>
2016-10-01 12:04:59 +0300

    Few methods were added

--- src/main/java/ua/softserve/rv_018/greentourism/config/authentication/TokenAuthenticationUtil.java
+++ src/main/java/ua/softserve/rv_018/greentourism/config/authentication/TokenAuthenticationUtil.java
@@ -2,25 +2,48 @@ package ua.softserve.rv_018.greentourism.config.authentication;
 
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import ua.softserve.rv_018.greentourism.model.User;
+import ua.softserve.rv_018.greentourism.service.UserService;
 
 @Component
 public class TokenAuthenticationUtil {
 
+	@Autowired
+	private UserService userService;
+	
 	/**
-	 * Tries to parse specified String as a JWT token. If successful, returns
-	 * User object with username, id and role prefilled (extracted from token).
-	 * If unsuccessful (token is invalid or not containing all required user
-	 * properties), simply returns null.
+	 *
+	 */
+	public static String encodeToken(Authentication token) {
+		return new String(Base64.getEncoder()
+				.encodeToString((token.getName() + ":" + (String) token.getCredentials()).getBytes()));
+	}
+	
+	/**
 	 * 
-	 * @param token
-	 *            the JWT token to parse
-	 * @return the User object extracted from specified token or null if a token
-	 *         is invalid.
+	 */
+	public void saveTokenToUser(Authentication authentication) {
+		User user = userService.findByEmail(authentication.getName());
+		user.setToken(encodeToken(authentication));
+		userService.update(user);
+	}
+	
+	/**
+	 * 
+	 */
+	public void deleteTokenFromUser(Authentication authentication) {
+		User user = userService.findByEmail(authentication.getName());
+		user.setToken(null);
+		userService.update(user);
+	}
+
+	/**
+	 * 
 	 */
 	public static UsernamePasswordAuthenticationToken genarateTokenFromData(String data) {
 		byte[] decoded = Base64.getDecoder().decode(data.substring(6));
@@ -44,17 +67,11 @@ public class TokenAuthenticationUtil {
 	 *            the user for which the token will be generated
 	 * @return the JWT token
 	 */
-	public static UsernamePasswordAuthenticationToken generateTokenFromCredentials(String email, String password) {
-		return new UsernamePasswordAuthenticationToken(email, password);
-	}
 
-	public static String createAuthorizationHeader(Authentication token) {
+	public static String generateAuthorizationHeaderFromToken(Authentication token) {
 		if ((token.getName() == null) || (token.getCredentials() == null))
 			return "No authorization";
-		String encoded = Base64.getEncoder()
-				.encodeToString((token.getName() + ":" + (String) token.getCredentials()).getBytes());
-		String header = "Basic " + encoded;
-		return header;
+		return "Basic " + encodeToken(token);
 	}
-	
+
 }
