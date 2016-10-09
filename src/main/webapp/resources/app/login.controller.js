2e0d94580be9f302724d202e412606ec0a810167
Andrew Kluyuko <akluyuko@gmail.com>
2016-10-09 11:36:54 +0300

    Now when user reloads page data is available

--- src/main/webapp/resources/app/login.controller.js
+++ src/main/webapp/resources/app/login.controller.js
@@ -3,8 +3,9 @@
 angular.module('greenApp').controller('loginController', function($scope, $rootScope, $http, $location, $localStorage) {
 	
 	$scope.loginCondition = $localStorage.message;
+	console.log('Authorization : ' + $localStorage.authorization);
 	console.log($localStorage.message);
-	if ($scope.loginCondition) {
+	if ($scope.loginCondition == null) {
 		$scope.loginCondition = 'login';
 	}
 	console.log($scope.loginCondition);
@@ -16,11 +17,11 @@ angular.module('greenApp').controller('loginController', function($scope, $rootS
 			url: _contextPath + "/login" + "?email=" + $scope.email + "&password=" + $scope.password
 		})
 		.then(function(response){
-			$rootScope.authorization = response.headers('Authorization'); 
-			$scope.email = "";
-			$scope.password = "";
-			$scope.loginCondition = "logout";
+			$localStorage.authorization = response.headers('Authorization'); 
+			$scope.email = '';
+			$scope.password = '';
 			$localStorage.message = 'logout';
+			$scope.loginCondition = $localStorage.message;
 		}, function(error){
 			console.log("Error in login function");
 			console.log(error.data);
@@ -32,18 +33,17 @@ angular.module('greenApp').controller('loginController', function($scope, $rootS
 		$http({
 			method: 'PATCH',
 			url: _contextPath + '/logout',
-			headers: { 'Authorization': $rootScope.authorization }
+			headers: { 'Authorization': $localStorage.authorization }
 		})
 		.then(function(response){
-			console.log("Success in logout function");
-			$scope.loginCondition = "login";
+			console.log('Success in logout function');
 			$localStorage.message = 'login';
+			$scope.loginCondition = $localStorage.message;
+			$localStorage.authorization = null;
 		}, function(error){
-			console.log("Error in logout function");
+			console.log('Error in logout function');
 			console.log(error.data);
 		});
-		
-		$scope.loginCondition = "login";
 	};
 	
 	// -----Forgot Password Functionality-----
